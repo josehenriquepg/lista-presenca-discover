36efdb97eb877e0fad92567ba96c1704b112b4ef
José Henrique <henricopg11@gmail.com>
2023-06-09 12:15:25 -0300

    Adicionando TypeScript

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,22 @@
 import { useState, useEffect } from 'react';
 import './style.css';
 
-import { Card } from '../../components/card';
+import { Card, CardProps } from '../../components/card';
+
+type ProfileResponse = {
+  name: string;
+  avatar_url: string;
+}
+
+type User = {
+  name: string;
+  avatar: string;
+}
 
 export function Home() {
-  const [studentName, setStudentName] = useState("");
-
-  const [students, setStudents] = useState([]);
-  const [user, setUser] = useState({ name: '', avatar: ''})
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState<CardProps[]>([]);
+  const [user, setUser] = useState<User>({} as User)
 
   function handleAddStudent() {
     const newStudent = {
@@ -19,13 +28,13 @@ export function Home() {
       }),
     };
 
-    setStudents((prevState) => [...prevState, newStudent]);
+    setStudents(prevState => [...prevState, newStudent]);
   }
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch('https://api.github.com/users/josehenriquepg');
-      const data = await response.json();
+      const data = await response.json() as ProfileResponse;
       setUser({
         name: data.name,
         avatar: data.avatar_url,
@@ -48,14 +57,18 @@ export function Home() {
       <input
         type="text"
         placeholder="Digite o nome..."
-        onChange={(e) => setStudentName(e.target.value)}
+        onChange={e => setStudentName(e.target.value)}
       />
       <button type="button" onClick={handleAddStudent}>
         Adicionar
       </button>
 
       {students.map((student) => (
-        <Card key={student.time} name={student.name} time={student.time} />
+        <Card 
+          key={student.time} 
+          name={student.name} 
+          time={student.time} 
+        />
       ))}
     </div>
   );
